9579b88f13ec0bb806a82d26ba0ce846dd8d45e7
Brad Powell <Brad.A.Powell@contractor.p66.com>
2021-02-25 17:43:10 -0600

    Refactoring to be a bit more 'pythonic'

--- sparkplug_b/stand_alone_examples/python/example.py
+++ sparkplug_b/stand_alone_examples/python/example.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python
-#/********************************************************************************
-# * Copyright (c) 2014, 2018 Cirrus Link Solutions and others
-# *
-# * This program and the accompanying materials are made available under the
-# * terms of the Eclipse Public License 2.0 which is available at
-# * http://www.eclipse.org/legal/epl-2.0.
-# *
-# * SPDX-License-Identifier: EPL-2.0
-# *
-# * Contributors:
-# *   Cirrus Link Solutions - initial implementation
-# ********************************************************************************/
+"""
+********************************************************************************
+* Copyright (c) 2014, 2018 Cirrus Link Solutions and others
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License 2.0 which is available at
+* http://www.eclipse.org/legal/epl-2.0.
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   Cirrus Link Solutions - initial implementation
+********************************************************************************
+"""
 import sys
-sys.path.insert(0, "../../../client_libraries/python/")
-#print(sys.path)
-
-import paho.mqtt.client as mqtt
-import sparkplug_b as sparkplug
 import time
 import random
 import string
-
+import paho.mqtt.client as mqtt
+sys.path.insert(0, "../../../client_libraries/python/")
+#print(sys.path)
+import sparkplug_b as sparkplug
 from sparkplug_b import *
 
-# Application Variables
-serverUrl = "localhost"
+# Application variables
+server_url = "localhost"
+server_port = 61883
+spb_namespace = "spBv1.0"
 myGroupId = "Sparkplug B Devices"
 myNodeName = "Python Edge Node 1"
 myDeviceName = "Emulated Device"
-publishPeriod = 5000
+#publishPeriod = 5000
 myUsername = "admin"
 myPassword = "changeme"
+client = None
 
 class AliasMap:
     Next_Server = 0
@@ -47,33 +50,37 @@ class AliasMap:
     Device_Metric3 = 11
     My_Custom_Motor = 12
 
-######################################################################
-# The callback for when the client receives a CONNACK response from the server.
-######################################################################
 def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected with result code "+str(rc))
-    else:
-        print("Failed to connect with result code "+str(rc))
+    """The callback for when the client receives a CONNACK response from the server.
+    """
+    print(f"on_connect(): result code = {rc} ({mqtt.connack_string(rc)})")
+    if rc != 0:
         sys.exit()
-
-    global myGroupId
-    global myNodeName
-
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("spBv1.0/" + myGroupId + "/NCMD/" + myNodeName + "/#")
-    client.subscribe("spBv1.0/" + myGroupId + "/DCMD/" + myNodeName + "/#")
-######################################################################
+    client.subscribe(spb_namespace + "/" + myGroupId + "/NCMD/" + myNodeName + "/#")
+    client.subscribe(spb_namespace + "/" + myGroupId + "/DCMD/" + myNodeName + "/#")
 
-######################################################################
-# The callback for when a PUBLISH message is received from the server.
-######################################################################
+def on_disconnect(client, userdata, rc):
+    """The callback for when the client is disconnected from the server.
+    """
+    print(f"on_disconnect(): result code = {rc} ({mqtt.connack_string(rc)})")
+
 def on_message(client, userdata, msg):
-    print("Message arrived: " + msg.topic)
-    tokens = msg.topic.split("/")
-
-    if tokens[0] == "spBv1.0" and tokens[1] == myGroupId and (tokens[2] == "NCMD" or tokens[2] == "DCMD") and tokens[3] == myNodeName:
+    """The callback for when a PUBLISH message is received from the server.
+    """
+    print(f"on_message(): message received: Topic: {msg.topic}, QoS: {msg.qos}, Payload Length: {len(msg.payload)}")
+    (namespace, group_id, message_type, *last) = msg.topic.split("/")
+    (edge_node_id, device_id) = (last[0], None if len(last) == 1 else last[1])
+    if namespace != spb_namespace:
+        print("Unknown/unsupported namespace: " + namespace)
+    elif group_id != myGroupId:
+        print("Unknown group_id: " + group_id)
+    elif message_type not in ("NCMD", "DCMD"):
+        print("Unsupported message_type: " + message_type)
+    elif edge_node_id != myNodeName:
+        print("Unknown edge_node_id: " + edge_node_id)
+    else:
         inboundPayload = sparkplug_b_pb2.Payload()
         inboundPayload.ParseFromString(msg.payload)
         for metric in inboundPayload.metrics:
@@ -82,7 +89,7 @@ def on_message(client, userdata, msg):
                 # disconnect from the current MQTT server and connect to the next MQTT server in the
                 # list of available servers.  This is used for clients that have a pool of MQTT servers
                 # to connect to.
-                print( "'Node Control/Next Server' is not implemented in this example")
+                print("'Node Control/Next Server' is not implemented in this example")
             elif metric.name == "Node Control/Rebirth" or metric.alias == AliasMap.Rebirth:
                 # 'Node Control/Rebirth' is an NCMD used to tell the device/client application to resend
                 # its full NBIRTH and DBIRTH again.  MQTT Engine will send this NCMD to a device/client
@@ -104,7 +111,7 @@ def on_message(client, userdata, msg):
 
                 # We know this is an Int16 because of how we declated it in the DBIRTH
                 newValue = metric.int_value
-                print( "CMD message for output/Device Metric2 - New Value: {}".format(newValue))
+                print("CMD message for output/Device Metric2 - New Value: {}".format(newValue))
 
                 # Create the DDATA payload - Use the alias because this isn't the DBIRTH
                 payload = sparkplug.getDdataPayload()
@@ -112,7 +119,7 @@ def on_message(client, userdata, msg):
 
                 # Publish a message data
                 byteArray = bytearray(payload.SerializeToString())
-                client.publish("spBv1.0/" + myGroupId + "/DDATA/" + myNodeName + "/" + myDeviceName, byteArray, 0, False)
+                client.publish(spb_namespace + "/" + myGroupId + "/DDATA/" + myNodeName + "/" + myDeviceName, byteArray, 0, False)
             elif metric.name == "output/Device Metric3" or metric.alias == AliasMap.Device_Metric3:
                 # This is a metric we declared in our DBIRTH message and we're emulating an output.
                 # So, on incoming 'writes' to the output we must publish a DDATA with the new output
@@ -121,7 +128,7 @@ def on_message(client, userdata, msg):
 
                 # We know this is an Boolean because of how we declated it in the DBIRTH
                 newValue = metric.boolean_value
-                print( "CMD message for output/Device Metric3 - New Value: %r" % newValue)
+                print("CMD message for output/Device Metric3 - New Value: %r" % newValue)
 
                 # Create the DDATA payload - use the alias because this isn't the DBIRTH
                 payload = sparkplug.getDdataPayload()
@@ -129,28 +136,31 @@ def on_message(client, userdata, msg):
 
                 # Publish a message data
                 byteArray = bytearray(payload.SerializeToString())
-                client.publish("spBv1.0/" + myGroupId + "/DDATA/" + myNodeName + "/" + myDeviceName, byteArray, 0, False)
+                client.publish(spb_namespace + "/" + myGroupId + "/DDATA/" + myNodeName + "/" + myDeviceName, byteArray, 0, False)
             else:
-                print( "Unknown command: " + metric.name)
-    else:
-        print( "Unknown command...")
+                #print("Unknown metric: " + repr(metric))
+                print(f"Unknown metric: name='{metric.name}', alias={metric.alias}")
 
-    print( "Done publishing")
-######################################################################
+def on_publish(client, userdata, mid):
+    """The callback for when a message has been published to the server.
+    """
+    print(f"on_publish(): message published: msg id = {mid}")
 
-######################################################################
-# Publish the BIRTH certificates
-######################################################################
+def on_subscribe(client, userdata, mid, granted_qos):
+    """The callback for when a server responds to a subscribe request.
+    """
+    print(f"on_subscribe(): subscribed: msg id = {mid}, granted_qos = {granted_qos}")
+
 def publishBirth():
+    """Publish the BIRTH certificates.
+    """
     publishNodeBirth()
     publishDeviceBirth()
-######################################################################
 
-######################################################################
-# Publish the NBIRTH certificate
-######################################################################
 def publishNodeBirth():
-    print( "Publishing Node Birth")
+    """Publish the NBIRTH certificate.
+    """
+    print("Publishing Node Birth")
 
     # Create the node birth payload
     payload = sparkplug.getNodeBirthPayload()
@@ -202,14 +212,12 @@ def publishNodeBirth():
 
     # Publish the node birth certificate
     byteArray = bytearray(payload.SerializeToString())
-    client.publish("spBv1.0/" + myGroupId + "/NBIRTH/" + myNodeName, byteArray, 0, False)
-######################################################################
+    client.publish(spb_namespace + "/" + myGroupId + "/NBIRTH/" + myNodeName, byteArray, 0, False)
 
-######################################################################
-# Publish the DBIRTH certificate
-######################################################################
 def publishDeviceBirth():
-    print( "Publishing Device Birth")
+    """Publish the DBIRTH certificate.
+    """
+    print("Publishing Device Birth")
 
     # Get the payload
     payload = sparkplug.getDeviceBirthPayload()
@@ -231,53 +239,56 @@ def publishDeviceBirth():
 
     # Publish the initial data with the Device BIRTH certificate
     totalByteArray = bytearray(payload.SerializeToString())
-    client.publish("spBv1.0/" + myGroupId + "/DBIRTH/" + myNodeName + "/" + myDeviceName, totalByteArray, 0, False)
-######################################################################
+    client.publish(spb_namespace + "/" + myGroupId + "/DBIRTH/" + myNodeName + "/" + myDeviceName, totalByteArray, 0, False)
+
 
 ######################################################################
 # Main Application
 ######################################################################
-print("Starting main application")
+if __name__ == '__main__':
+    print("Starting main application")
 
-# Create the node death payload
-deathPayload = sparkplug.getNodeDeathPayload()
+    # Create the node death payload
+    deathPayload = sparkplug.getNodeDeathPayload()
 
-# Start of main program - Set up the MQTT client connection
-client = mqtt.Client(serverUrl, 1883, 60)
-client.on_connect = on_connect
-client.on_message = on_message
-client.username_pw_set(myUsername, myPassword)
-deathByteArray = bytearray(deathPayload.SerializeToString())
-client.will_set("spBv1.0/" + myGroupId + "/NDEATH/" + myNodeName, deathByteArray, 0, False)
-client.connect(serverUrl, 1883, 60)
+    # Start of main program - Set up the MQTT client connection
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_message = on_message
+    client.on_publish = on_publish
+    client.on_subscribe = on_subscribe
+    client.username_pw_set(myUsername, myPassword)
+    deathByteArray = bytearray(deathPayload.SerializeToString())
+    client.will_set(spb_namespace + "/" + myGroupId + "/NDEATH/" + myNodeName, deathByteArray, 0, False)
+    client.connect(server_url, server_port, 60)
 
-# Short delay to allow connect callback to occur
-time.sleep(.1)
-client.loop()
+    # Short delay to allow connect callback to occur
+    time.sleep(0.1)
+    client.loop()
 
-# Publish the birth certificates
-publishBirth()
+    # Publish the birth certificates
+    publishBirth()
 
-while True:
-    # Periodically publish some new data
-    payload = sparkplug.getDdataPayload()
+    while True:
+        # Periodically publish some new data
+        payload = sparkplug.getDdataPayload()
 
-    # Add some random data to the inputs
-    addMetric(payload, None, AliasMap.Device_Metric0, MetricDataType.String, ''.join(random.choice(string.ascii_lowercase) for i in range(12)))
+        # Add some random data to the inputs
+        addMetric(payload, None, AliasMap.Device_Metric0, MetricDataType.String, ''.join(random.choice(string.ascii_lowercase) for i in range(12)))
 
-    # Note this data we're setting to STALE via the propertyset as an example
-    metric = addMetric(payload, None, AliasMap.Device_Metric1, MetricDataType.Boolean, random.choice([True, False]))
-    metric.properties.keys.extend(["Quality"])
-    propertyValue = metric.properties.values.add()
-    propertyValue.type = ParameterDataType.Int32
-    propertyValue.int_value = 500
+        # Note this data we're setting to STALE via the propertyset as an example
+        metric = addMetric(payload, None, AliasMap.Device_Metric1, MetricDataType.Boolean, random.choice([True, False]))
+        metric.properties.keys.extend(["Quality"])
+        propertyValue = metric.properties.values.add()
+        propertyValue.type = ParameterDataType.Int32
+        propertyValue.int_value = 500
 
-    # Publish a message data
-    byteArray = bytearray(payload.SerializeToString())
-    client.publish("spBv1.0/" + myGroupId + "/DDATA/" + myNodeName + "/" + myDeviceName, byteArray, 0, False)
+        # Publish a message
+        byteArray = bytearray(payload.SerializeToString())   # SerializeToString() returns a bytes type
+        client.publish(spb_namespace + "/" + myGroupId + "/DDATA/" + myNodeName + "/" + myDeviceName, byteArray, 0, False)
 
-    # Sit and wait for inbound or outbound events
-    for _ in range(5):
-        time.sleep(.1)
-        client.loop()
-######################################################################
+        # Sit and wait for inbound or outbound events
+        for _ in range(5):
+            time.sleep(0.1)
+            client.loop()
